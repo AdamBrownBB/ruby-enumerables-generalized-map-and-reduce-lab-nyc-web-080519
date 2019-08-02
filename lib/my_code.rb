6c0eeff7c828a6754b132724f50500355fb8ec1c
Adam Brown <adam.brown.betty@gmail.com>
2019-08-02 15:05:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,7 @@ return new_array
 end 
 
 
-def reduce(source_array)
-  
-  def reduce_to_total(source_array, starting_point=0)
+def reduce(source_array, starting_point)
  i = 0 
  result = starting_point
     while i < source_array.length do
@@ -21,16 +19,3 @@ def reduce(source_array)
   end 
     return result
   end 
-
-
-def reduce_to_all_true(source_array)
-   i = 0 
-    while i < source_array.length do
-     if !source_array[i] 
-     return false
-    end 
-  i +=1 
-  
- end 
- return true 
-end 
